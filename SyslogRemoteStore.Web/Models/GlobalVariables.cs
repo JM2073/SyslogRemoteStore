13d5d2f04047cb1251bf0180daceac255d00b530
a0810 <ao810@student.aru.ac.uk>
2023-10-26 09:32:02 +0100

    Created a Class to strore our global variables such as the default ip and port. Feel free to add more ase we move along

---
+++ SyslogRemoteStore.Web/Models/GlobalVariables.cs
@@ -0,0 +1,12 @@
+﻿namespace SyslogRemoteStore.Web.Models;
+
+public class GlobalVariables
+{
+    public  string DefaultIp { get; set; }
+    public int DefaultPort { get; set; }
+    public string WarningHex { get; set; }
+    public string ErrorHex { get; set; }
+    public int DebugHex { get; set; }
+    public int InfoHex { get; set; }
+
+}
