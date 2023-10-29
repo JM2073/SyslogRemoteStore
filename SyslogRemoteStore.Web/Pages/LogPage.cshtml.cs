09ec54f0395b8e8bacdcd2d416e8c24df615ff8d
MattClark94 <mc1556@student.aru.ac.uk>
2023-10-29 14:47:03 +0000

    Creation of LogPage and Settings Page

---
+++ SyslogRemoteStore.Web/Pages/LogPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SyslogRemoteStore.Web.Pages
+{
+    public class LogPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ SyslogRemoteStore.Web/Pages/SettingsPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SyslogRemoteStore.Web.Pages
+{
+    public class SettingsPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
