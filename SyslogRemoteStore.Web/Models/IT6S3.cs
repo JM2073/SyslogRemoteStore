2b547c0bba38df1ba22b97ec91156cfc9d87d37d
Johnathon Millard <JM2073@student.aru.ac.uk>
2023-11-01 16:46:08 +0000

    removed the weather stuff, it is no longer needed refactored the radio class, its not pritty, could be split into a handler class but will review latter connected RadioPage.razor to backend now supports IPV6 aswell as IPV4

--- SyslogRemoteStore.Web/Models/IT6S3.cs
+++ SyslogRemoteStore.Web/Models/IT6S3.cs
@@ -1,15 +1,22 @@
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Net.Sockets;
 
 namespace SyslogRemoteStore.Web.Models;
 
 public interface IT6S3
 {
+    event PropertyChangedEventHandler? PropertyChanged;
+
+    bool TcpConnected { get; }
+    bool AlertFlag { get; }
+    Guid Id { get; }
     string Ip { get; }
     int Port { get; }
-    bool AlertFlag { get; }
-    
     Socket Socket { get; }
     ObservableCollection<Log> Logs { get; }
 
+    string GetIpvType();
+    string GetFormatedIp();
+    string GetSocketStatus();
 }
