093ff0ceaebe3224f18adcec27f2abead1a4f2fd
a0810 <ao810@student.aru.ac.uk>
2023-11-02 14:36:56 +0000

    LogFilter

--- SyslogRemoteStore.Web/Services/LogFilterService.cs
+++ SyslogRemoteStore.Web/Services/LogFilterService.cs
@@ -1,34 +1,11 @@
-﻿using System.Net;
+﻿using SyslogRemoteStore.Web.Models;
+using System.ComponentModel;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
 
-namespace SyslogRemoteStore.Web.ViewModels
+namespace SyslogRemoteStore.Web.Services
 {
-    public class FilterViewModel
-    {
-        public string SourceIp { get; set; }
-        public string Severity { get; set; }
-    }
-
-    private void Filter()
-    {
-        public List<Log> FilterLog(List<Log> logs, FilterViewModel filter)
-        {
-            var filteredLogs = logs;
-
-            if (!string.IsNullOrEmpty(filter.SourceIp))
-            {
-                filteredLogs = filteredLogs.Where(l => l.SourceIp.Contains(filter.SourceIp)).ToList();
-            }
-
-            if (!string.IsNullOrEmpty(filter.Severity))
-            {
-                filteredLogs = filteredLogs.Where(l => l.Severity.Contains(filter.Severity)).ToList();
-            }
-
-            return filteredLogs;
-        }
-    }
-
-    public class FilterViewModel : INotifyPropertyChanged
+    public class FilterService : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -37,7 +14,42 @@ namespace SyslogRemoteStore.Web.ViewModels
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
+        public string SourceIp { get; set; }
+        public string Severity { get; set; }
+
+
+        private void Filter()
+        {
+            List<Log> FilterLog(List<Log> logs, FilterService filter)
+            {
+                var filteredLogs = logs;
+
+                if (!string.IsNullOrEmpty(filter.SourceIp))
+                {
+                    filteredLogs = filteredLogs.Where(l => l.SourceIp.Contains(filter.SourceIp)).ToList();
+                }
+
+                if (!string.IsNullOrEmpty(filter.Severity))
+                {
+                    filteredLogs = filteredLogs.Where(l => l.Severity.Contains(filter.Severity)).ToList();
+                }
+
+                return filteredLogs;
+            }
+        }
     }
 }
+
+  /*   public class FilterService : INotifyPropertyChanged
+  {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }*/
+
+ 
+
 
 
