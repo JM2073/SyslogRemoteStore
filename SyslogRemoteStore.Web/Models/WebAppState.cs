4182e119cfcbf0b7fbe6fe5fb2750c16f405a48c
MattClark94 <mc1556@student.aru.ac.uk>
2023-11-23 00:47:02 +0000

    Not the final draft but 90% there.

---
+++ SyslogRemoteStore.Web/Models/WebAppState.cs
@@ -0,0 +1,18 @@
+﻿namespace SyslogRemoteStore.Web.Models;
+
+public class WebAppState
+{
+    public bool NavState = false;
+
+    public WebAppState(){}
+
+	public void UpdateNavState()
+	{
+        NavState = NavState switch
+        {
+            false => true,
+            _ => false,
+        };
+    }
+
+}
